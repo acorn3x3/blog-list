9ce687e6eca3ebcdaa61b549c3dec18bd57967ba
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-17 16:06:34 -0800

    Main.js returns mapped blogs. useBlogs hook now draws in Blog data

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getBlogs } from '../services/blog.js';
+import { getBlogs } from '../services/blogs.js';
 
 export function useBlogs() {
   const [blogs, setBlogs] = useState([]);
